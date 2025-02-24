78a762066db3e1cb6252277842bb5a8a049e38b4
Yaroslav <117809963+Yaroslav-fashevskyi@users.noreply.github.com>
2025-02-24 09:59:27 +0200

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,22 @@
+text1 = input("Enter a string: ")
+text2 = input("Enter another string: ")
+text3 = input("Enter another string: ")
+textadd = text1 +" "+ text2 +" "+ text3
+
+print(textadd)
+
+duplicate = int(input("How many times the row should be duplicated: "))
+
+duplicated = textadd * duplicate
+print(duplicated)
+
+І
+
+'''Завдання 1.
+Завдання 1.
+Напишіть програму, в якій користувач вводить три рядкових значення. 
+Обʼєднайте ці значення, розділивши їх через пробіл та виведіть в консоль.
+ Після цього запитайте в користувача, скільки разів потрібно продублювати рядок 
+ і виконайте цю операцію з обʼднаним рядком та виведіть його в консоль.
+
+'''
---
+++ p5.py
@@ -0,0 +1,13 @@
+string_1 = input("Enter a string: ")
+number_n = int(input("Enter a number: "))
+
+print(string_1[number_n:-number_n])
+
+
+
+
+'''
+Напишіть програму, яка приймає рядок та ціле число n і повертає новий рядок,
+ у якому видалені перші та останні n символів.. Наприклад, для рядка
+  "abcdefg" і n = 2 результатом буде "cde".
+'''
