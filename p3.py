896f32d42b77461eba34e062ba13e6304fa25bd3
Yaroslav <yaroslav.i.fashevskyi@ukd.edu.ua>
2025-03-07 12:55:45 +0200

    up

--- p3.py
+++ p3.py
@@ -3,8 +3,7 @@ def print_student_info(**kwargs):
       print("Немає інформації про студента")
   else:
       for key, value in kwargs.items():
-          up_key = key.capitalize()
-          print(f"{up_key}: {value}")
+          print(f"{key.capitalize()}: {value}")
 
 if __name__ == '__main__':
         print_student_info(name="Ярослав", age=17, course=3, group="КІПЗс-22-3")
